bd2ba19d4c68b688c06a6a492fe878d546bf453a
fullyalive <wogud1202@gmail.com>
2018-05-10 09:33:34 +0900

    portals

--- master-react16/src/App.js
+++ master-react16/src/App.js
@@ -1,10 +1,20 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
+import { createPortal } from "react-dom";
+
+class Portals extends Component {
+  render() {
+    return createPortal(<Message />, document.getElementById("touchme"));
+  }
+}
+
+const Message = () => "Just touched it!";
 
 class ReturnTypes extends Component {
-  render(){
+  render() {
     return "hello";
   }
 }
+
 class App extends Component {
   render() {
     return (
